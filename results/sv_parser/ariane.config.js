f1d88b3890b4200bae43bcfee52493e7626aef51
SymbiFlow Robot <foss-fpga-tools@google.com>
2023-08-24 02:49:56 +0000

    Deploy  (build 5958297172) Build from e609910a9ff08665091a47532ab8be48c2abd192

--- results/sv_parser/ariane.config.js
+++ results/sv_parser/ariane.config.js
@@ -1,1 +1,1 @@
-config_loader_data['sv_parser/ariane'] = [["cores","ariane",0,"logs/sv_parser/generated/ariane/ariane.sv.log.html","third_party/cores/ariane/vendor/pulp-platform/fpga-support/rtl/SyncDpRam.sv.html"]]
+config_loader_data['sv_parser/ariane'] = [["cores","ariane",1,"logs/sv_parser/generated/ariane/ariane.sv.log.html","third_party/cores/ariane/vendor/pulp-platform/fpga-support/rtl/SyncDpRam.sv.html"]]
