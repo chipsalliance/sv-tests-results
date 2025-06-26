00d2590e0e21be5df1eb9f37a322fdd3a4dc4972
SymbiFlow Robot <foss-fpga-tools@google.com>
2025-06-26 03:05:45 +0000

    Deploy refs/heads/master (build 15890840679) Build from 9affadd5679fde4e34a8fe8aab56c6e9ec429aa1

--- results/circt_verilog/7.7.config.js
+++ results/circt_verilog/7.7.config.js
@@ -1,1 +1,1 @@
-config_loader_data['circt_verilog/7.7'] = [["","unpacked-arrays-as-arguments-to-subroutines",1,"logs/circt_verilog/chapter-7/arrays/unpacked/subroutines.sv.log.html","tests/chapter-7/arrays/unpacked/subroutines.sv.html"]]
+config_loader_data['circt_verilog/7.7'] = [["","unpacked-arrays-as-arguments-to-subroutines",0,"logs/circt_verilog/chapter-7/arrays/unpacked/subroutines.sv.log.html","tests/chapter-7/arrays/unpacked/subroutines.sv.html"]]
