fd00d82963366d18dcc666f7083daf43bef30e57
SymbiFlow Robot <foss-fpga-tools@google.com>
2026-03-28 03:10:54 +0000

    Deploy refs/heads/master (build 23674485689) Build from 3d9bf0ac8d485425be3a1e9c6a88c51e604350b2

--- results/circt_verilog/7.3.1.config.js
+++ results/circt_verilog/7.3.1.config.js
@@ -1,1 +1,1 @@
-config_loader_data['circt_verilog/7.3.1'] = [["","basic-packed-unions",0,"logs/circt_verilog/chapter-7/unions/packed/basic.sv.log.html","tests/chapter-7/unions/packed/basic.sv.html"]]
+config_loader_data['circt_verilog/7.3.1'] = [["","basic-packed-unions",1,"logs/circt_verilog/chapter-7/unions/packed/basic.sv.log.html","tests/chapter-7/unions/packed/basic.sv.html"]]
