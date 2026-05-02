af87673e1a514d20905158ff7bc621400bf652c0
SymbiFlow Robot <foss-fpga-tools@google.com>
2026-05-02 03:11:37 +0000

    Deploy refs/heads/master (build 25240465917) Build from 1db8a9ae0958d4f93207c9469080c1ab551212ba

--- results/circt_verilog/6.24.2.config.js
+++ results/circt_verilog/6.24.2.config.js
@@ -1,1 +1,1 @@
-config_loader_data['circt_verilog/6.24.2'] = [["","cast_fn",0,"logs/circt_verilog/chapter-6/6.24.2--cast_fn.sv.log.html","tests/chapter-6/6.24.2--cast_fn.sv.html"],["","cast_task",0,"logs/circt_verilog/chapter-6/6.24.2--cast_task.sv.log.html","tests/chapter-6/6.24.2--cast_task.sv.html"]]
+config_loader_data['circt_verilog/6.24.2'] = [["","cast_fn",1,"logs/circt_verilog/chapter-6/6.24.2--cast_fn.sv.log.html","tests/chapter-6/6.24.2--cast_fn.sv.html"],["","cast_task",1,"logs/circt_verilog/chapter-6/6.24.2--cast_task.sv.log.html","tests/chapter-6/6.24.2--cast_task.sv.html"]]
